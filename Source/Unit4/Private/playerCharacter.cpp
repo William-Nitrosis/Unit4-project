3626c0a5c701f059beca992814815d542e0a2984
William-Nitrosis <33815671+William-Nitrosis@users.noreply.github.com>
2019-02-01 17:00:04 +0000

    State machine

--- Source/Unit4/Private/playerCharacter.cpp
+++ Source/Unit4/Private/playerCharacter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "playerCharacter.h"
+#include "enemyAi.h"
 
 
 // Sets default values
--- Source/Unit4/Public/enemyAi.h
+++ Source/Unit4/Public/enemyAi.h
@@ -6,6 +6,53 @@
 #include "GameFramework/Character.h"
 #include "enemyAi.generated.h"
 
+
+//If you want this to appear in BP, make sure to use this instead
+//USTRUCT(BlueprintType)
+USTRUCT()
+struct FPlayerStateStruct
+{
+	GENERATED_BODY()
+
+		//Always make USTRUCT variables into UPROPERTY()
+		//    any non-UPROPERTY() struct vars are not replicated
+
+		// So to simplify your life for later debugging, always use UPROPERTY()
+		UPROPERTY()
+		int32 SampleInt32;
+
+	//If you want the property to appear in BP, make sure to use this instead
+	//UPROPERTY(BlueprintReadOnly)
+	UPROPERTY()
+		AActor* TargetActor;
+
+	//Constructor
+	FPlayerStateStruct()
+	{
+		//Always initialize your USTRUCT variables!
+		//   exception is if you know the variable type has its own default constructor
+		SampleInt32 = 5;
+		TargetActor = NULL;
+	}
+};
+
+USTRUCT()
+struct FPlayerStateStructInherited : public FPlayerStateStruct
+{
+	GENERATED_BODY()
+
+
+	//Constructor
+	FPlayerStateStructInherited()
+	{
+		//Always initialize your USTRUCT variables!
+		//   exception is if you know the variable type has its own default constructor
+		SampleInt32 = 5;
+		TargetActor = NULL;
+	}
+};
+
+
 UCLASS()
 class UNIT4_API AenemyAi : public ACharacter
 {
@@ -14,6 +61,11 @@ class UNIT4_API AenemyAi : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AenemyAi();
+
+	//this is the highlander!
+	static FPlayerStateStruct player_struct;
+	static FPlayerStateStructInherited struct2;
+
 
 protected:
 	// Called when the game starts or when spawned
@@ -29,3 +81,4 @@ public:
 	
 	
 };
+
